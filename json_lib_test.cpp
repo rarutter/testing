14fee3701782be3e39c04c372e3880d7b71f2d5e
Ruth Rutter <ruth@localhost.admin.uaf.edu>
2014-02-28 11:11:34 -0900

    random numbers, writes single iteration to file

--- json_lib_test.cpp
+++ json_lib_test.cpp
@@ -2,21 +2,40 @@
 
 #include <iostream>
 #include <json/writer.h>
+#include <cstdlib>
+#include <random>
+#include <functional> //for std::bind
+#include <fstream>
 
 int main(){
 
-    Json::Value event;
-    Json::Value vec(Json::arrayValue);
-    vec.append(Json::Value(1));
-    vec.append(Json::Value(2));
-    vec.append(Json::Value(3));
+    Json::Value data;
 
-    event["competitors"]["home"]["name"] = "Liverpool";
-    event["competitors"]["away"]["code"] = 89223;
-    event["competitors"]["away"]["name"] = "Aston Villa";
-    event["competitors"]["away"]["code"]=vec;
+    srand (time(NULL));
 
-    std::cout << event << std::endl;
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1,20);
+    auto roll = std::bind(distribution, generator);
+
+    std::ofstream out_stream("output.json", std::ofstream::out);
+
+    for(int ii=0; ii<1; ii++){
+   
+        //Monthly Thermal information 
+        data["TempAir"] = roll();
+        data["TempOrganicLayer"] = roll();
+        data["TempMineralLayer"] = roll(); 
+        data["PAR"] = roll();
+        data["ActiveLayerDepth"] = roll();
+
+        //Monthly Hydrodynamic information
+        //etc...
+
+        out_stream << data << std::endl;
+
+    }
+
+    out_stream.close();
 
     return 0;
 }
