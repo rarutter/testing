5f9431655c50bebabff599d06498e5f1d8a1249a
Ruth Rutter <ruth@localhost.admin.uaf.edu>
2014-02-28 13:35:39 -0900

    saving this py example. Triggers too often, or resolves more than once per trigger

--- json_plot.py
+++ json_plot.py
@@ -1,11 +1,56 @@
+import sys
+
 import json
 from pprint import pprint
 
-json_data = open('output.json');
+import time #for sleep
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import FileModifiedEvent
 
-data = json.load(json_data);
-pprint(data);
+class Handler(PatternMatchingEventHandler):
+    patterns = ["*.json"]
 
-json_data.close();
+    def process(self, event):
+        """
+        event.event_type 
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+        # the file will be processed there
+        print event.src_path, event.event_type  # print now only for degug
 
-print data["PAR"] 
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    observer = Observer()
+    observer.schedule(Handler(), path=args[0] if args else '.')
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
+
+
+#json_data = open('output.json');
+
+#data = json.load(json_data);
+#pprint(data);
+
+#json_data.close();
+
+#print data["PAR"] 
